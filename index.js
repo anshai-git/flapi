4055344c062d5d5fc985914728f3956f47f668f1
Kovacs Ervin <kov.ervin97@gmail.com>
2023-07-11 09:47:00 +0300

    now in blue

--- index.js
+++ index.js
@@ -10,10 +10,13 @@ class Pipe {
     pos_x;
     pos_y;
     counted = false;
+    color;
 
     constructor(x, y) {
         this.pos_x = x;
         this.pos_y = y;
+        this.counted = false;
+        this.color = generate_blue_color();
     }
 }
 
@@ -41,8 +44,14 @@ document.addEventListener("keydown", (event) => {
         // } break;
 
         case 'KeyQ': {
+            if (pipe_boost > 0) return;
+
             pipe_boost = 15;
             jump_v_vel = 100;
+            pipes.push( new Pipe(canvas.offsetWidth + 50, random(5, 151)));
+            pipes.push( new Pipe(canvas.offsetWidth + 150, random(5, 151)));
+            pipes.push( new Pipe(canvas.offsetWidth + 240, random(5, 151)));
+            pipes.push( new Pipe(canvas.offsetWidth + 370, random(5, 151)));
         } break;
 
         default: {
@@ -53,8 +62,11 @@ document.addEventListener("keydown", (event) => {
 
 function render() {
     const circle = new Path2D();
+    ctx.fillStyle = 'rgb(0, 0, 0)';
     ctx.clearRect(0, 0, canvas.offsetWidth, canvas.offsetHeight);
     circle.arc(pos_x, pos_y, 25, 0, 2*Math.PI);
+    ctx.fill(circle);
+
 
     pos_y += (horizontal_velocity + jump_h_vel);
     if (jump_h_vel < 0) jump_h_vel += -jump_h_vel/50;
@@ -66,15 +78,16 @@ function render() {
 
     if (pos_x > 100) pos_x -= 1;
 
-    pipes = pipes.filter(p => p.pos_x > -50);
+    pipes = pipes.filter(p => p.pos_x > -100);
     
     for (pipe of pipes) {
         const top = new Path2D();
         const bottom = new Path2D();
 
-        top.rect(pipe.pos_x, 0, 50, (canvas.offsetHeight/2) - pipe.pos_y);
-        bottom.rect(pipe.pos_x, canvas.offsetHeight - pipe.pos_y,  50, 200);
-
+        top.rect(pipe.pos_x, 0, 50, (canvas.offsetHeight/3) - pipe.pos_y);
+        bottom.rect(pipe.pos_x, canvas.offsetHeight - pipe.pos_y*2,  50, 500);
+        
+        ctx.fillStyle = pipe.color;
         ctx.fill(top);
         ctx.fill(bottom);
         
@@ -86,22 +99,41 @@ function render() {
         pipe.pos_x -= 2 + pipe_boost;
     }
 
-    if(pipe_boost > 0) pipe_boost -= pipe_boost/50;
+    ctx.fillStyle = 'rgb(0, 0, 0)';
+    ctx.font = 'bold 80px Arial';
+    ctx.textAlign = 'center';
+    ctx.fillText(score, canvas.offsetWidth/2 - 25, 80);
 
-    ctx.fill(circle);
+    if (pipe_boost > 0) pipe_boost -= pipe_boost/50;
+    if (pipe_boost < 1) pipe_boost = 0;
+
     requestAnimationFrame(render);
-}
-
-function create_pipe(){
-    pipes.push( new Pipe(canvas.offsetWidth, random(5, 151)));
-
-    setTimeout(create_pipe, random(500, 2000));
 }
 
 create_pipe();
 
+function create_pipe(){
+    console.log(generate_blue_color());
+    pipes.push( new Pipe(canvas.offsetWidth, random(5, 151)));
+    setTimeout(create_pipe, random(300, 1000));
+}
+
+function generate_blue_color() {
+    return generate_color([10, 50], [80, 120], [200, 220], [4, 8]);
+}
+
+function generate_color(red_range, green_range, blue_range, alpha_range) {
+    let red     = Math.ceil(random(red_range[0], red_range[1]));
+    let green   = Math.ceil(random(green_range[0], green_range[1]));
+    let blue    = Math.ceil(random(blue_range[0], blue_range[1]));
+    let alpha   = Math.ceil(random(alpha_range[0], alpha_range[1]));
+    return `rgba(${red}, ${green}, ${blue}, 0.${alpha})`;
+}
+
 function random(min, max) {
     return Math.random() * (max - min) + min;
 }
+
+
 
 requestAnimationFrame(render);
